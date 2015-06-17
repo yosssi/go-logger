6c9528dcf880b5fb7463c288955fcf9acabc3ac8
yosssi <yoshida.keiji.84@gmail.com>
2015-06-17 17:10:05 +0900

    Update files

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# go-logger
-Go Logger
+# Go Logger
---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package logger provides a logger which output logs
+// at various levels such as trace, debug, info, warn
+// and error.
+package logger
